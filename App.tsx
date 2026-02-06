89451d9715e4c312a910e758df5ea83599f5689c
rawilliams0407 <rawilliams0407@gmail.com>
2026-02-06 16:48:36 -0500

    Refactor: Update App structure and Home page with lineup filtering and JSON data source

--- App.tsx
+++ App.tsx
@@ -7,17 +7,26 @@ import { Home } from "./pages/Home";
 import { Shop } from "./pages/Shop";
 import { About } from "./pages/About";
 import { Contact } from "./pages/Contact";
+import { CheckoutPage } from "./pages/CheckoutPage";
+import { SuccessPage } from "./pages/SuccessPage";
+import { ScrollToTop } from "./components/ScrollToTop";
 
 export const App = () => {
     return (
         <BrowserRouter>
+            <ScrollToTop />
             <Routes>
+                {/* Pages using shared Layout */}
                 <Route element={<Layout />}>
                     <Route path="/" element={<Home />} />
                     <Route path="/shop" element={<Shop />} />
                     <Route path="/about" element={<About />} />
                     <Route path="/contact" element={<Contact />} />
                 </Route>
+
+                {/* Independent Pages */}
+                <Route path="/checkout" element={<CheckoutPage />} />
+                <Route path="/success" element={<SuccessPage />} />
             </Routes>
         </BrowserRouter>
     );
